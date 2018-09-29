1dc246477c1599998319f64f4cbe8a2b97c98520
Cody Michael Simmons <csimmons@roirevolution.com>
2018-09-28 22:32:02 -0400

    Adds Jasmine as our testing solution.

---
+++ spec/jasmine_spec.js
@@ -0,0 +1,9 @@
+// This is only a test file to ensure Jasmine is running properly. Do not modify this file.
+describe("Jasmine Test", function() {
+    it("should be truthy when true", function() {
+        expect(true).toBeTruthy();
+    });
+    it("should be falsy when false", function() {
+        expect(false).toBeFalsy();
+    });
+});
