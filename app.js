ac767ee5138e1c1d9d1787afffdfd105d42b93c7
CodyMichaelSimmons <codyco@live.unc.edu>
2018-10-13 12:21:04 -0400

    Enables compiling for .scss files.

--- app.js
+++ app.js
@@ -26,6 +26,12 @@ app.use(sassMiddleware({
   indentedSyntax: true, // true = .sass and false = .scss
   sourceMap: true
 }));
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: false, // true = .sass and false = .scss
+  sourceMap: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
