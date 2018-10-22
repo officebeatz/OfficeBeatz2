54035eae5a31de37fe710ce4a4afc8380defc3c4
CodyMichaelSimmons <codyco@live.unc.edu>
2018-10-22 16:38:11 -0400

    Base looping functionality.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,8 +6,26 @@ $(document).ready(function () {
     var audioElement = $('#audioSource')[0]
 
     $('#play').click(function () {
+        audioElement.autoplay = true;
         audioElement.play();
+        setInterval(function () {
+            loopPlayer(audioElement);
+        }, 5000);
+
     });
+
+    function loopPlayer(audioElement) {
+        $.ajax({
+            url: '/api/next',
+            data: null,
+            type: "POST",
+            success: function (responseData) {
+                console.log(responseData);
+                audioElement.src = responseData;
+            }, error: console.error
+        });
+        audioElement.play();
+    }
 
     $('#pause').click(function () {
         audioElement.pause();
