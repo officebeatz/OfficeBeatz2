0f4af37097f74fb94033998de3fbc0dd4c299059
CodyMichaelSimmons <codyco@live.unc.edu>
2018-10-22 20:38:01 -0400

    Changes interval and cleans up looping functionality.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,17 +1,20 @@
-// Initializes SideNav for mobile
 $(document).ready(function () {
     $('.sidenav').sidenav();
     $('.tabs').tabs();
 
-    var audioElement = $('#audioSource')[0]
+    const TIME_INTERVAL = 15000; // 15 Seconds for testing purposes
+
+    var audioElement = $('#audioSource')[0]; // jQuery syntax to grab the first child of the audio object
 
     $('#play').click(function () {
+        // Updates autoplay after an action has taken place
         audioElement.autoplay = true;
         audioElement.play();
+
+        // Makes an API request for a new song every TIME_INTERVAL milliseconds
         setInterval(function () {
             loopPlayer(audioElement);
-        }, 5000);
-
+        }, TIME_INTERVAL);
     });
 
     function loopPlayer(audioElement) {
